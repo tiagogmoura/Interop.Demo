1f2e567c4893db5f2e3c79ae97f58aacfc79ab30
Tiago Moura <tgmoura@gmail.com>
2017-10-16 22:16:05 -0200

    Add validations #4

--- source/InteropDemo.Web/Models/CourseViewModel.cs
+++ source/InteropDemo.Web/Models/CourseViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace InteropDemo.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InteropDemo.Web.Models
 {
     public class CourseViewModel
 	{
 		public int Id { get; set; }
+		
+		[Required]
 		public string Title { get; set; }
 		public string Description { get; set; }
+		[Required]
 		public string Room { get; set; }
 		public double Investment { get; set; }
 	}
--- source/InteropDemo.Web/Models/StudentViewModel.cs
+++ source/InteropDemo.Web/Models/StudentViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace InteropDemo.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InteropDemo.Web.Models
 {
     public class StudentViewModel
     {
 		public int Id { get; set; }
-	    public string Name { get; set; }
-	    public string Email { get; set; }
+	    
+		[Required]
+		public string Name { get; set; }
+
+	    [Required]
+		[EmailAddress(ErrorMessage = "E-mail inválido")]
+		public string Email { get; set; }
 	    public string Phone { get; set; }
 	    public int CourseId { get; set; }
 	    public virtual CourseViewModel Course { get; set; }
